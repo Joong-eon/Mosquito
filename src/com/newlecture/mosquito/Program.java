f23afb7c8e484d3e439e817a15a15b23fc8eadec
min-96 <ajdcnd131@naver.com>
2020-10-09 16:00:35 +0900

    111111

--- src/com/newlecture/mosquito/Program.java
+++ src/com/newlecture/mosquito/Program.java
@@ -5,11 +5,6 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello~!~!~!!");
-		System.out.println("im jun~");
-		System.out.println("1111111");
-		System.out.println("제발 되어라");
-		System.out.println("되냐?");
 	}
 
 }
