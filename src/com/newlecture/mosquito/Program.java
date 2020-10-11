bf2f09fd85eddcc33b97f3e765c876607c9d88ee
min-96 <ajdcnd131@naver.com>
2020-10-11 12:22:53 +0900

    test

--- src/com/newlecture/mosquito/Program.java
+++ src/com/newlecture/mosquito/Program.java
@@ -4,9 +4,11 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2��");
 		
-	
+		
+		System.out.println("�躴�� ");
+		System.out.println("Thanks!");
+		System.out.println("안녕안녕 >ㅠ<");
 	}
 
 }
