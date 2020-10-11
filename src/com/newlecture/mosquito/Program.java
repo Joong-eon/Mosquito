8afdbcda986ad1a827beda8ce8ea136558ccb8e0
fligh <fligh@DESKTOP-G9JBLAI>
2020-10-11 11:29:09 +0900

    1112

--- src/com/newlecture/mosquito/Program.java
+++ src/com/newlecture/mosquito/Program.java
@@ -4,11 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("2Á¶");
 		
-		
-		System.out.println("ï¿½èº´ï¿½ï¿½ ");
-		System.out.println("Thanks!");
-		System.out.println("´Ï³ª³ë");
+	
 	}
 
 }
