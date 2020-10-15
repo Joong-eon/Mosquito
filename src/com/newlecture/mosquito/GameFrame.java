400ff57799746b381d921f48f3b0ff933d0110e8
hi <hi@DESKTOP-6RL4QDK>
2020-10-15 13:18:32 +0900

    [김동영] GameFrame 캔버스 사이즈 전역변수 설정

--- src/com/newlecture/mosquito/GameFrame.java
+++ src/com/newlecture/mosquito/GameFrame.java
@@ -5,15 +5,18 @@ import java.awt.Frame;
 import com.newlecture.mosquito.canvas.StageCanvas;
 
 public class GameFrame extends Frame {
-	
+
+	public static int canvasWidth = 500;
+	public static int canvasHeight = 500;
+
 	public GameFrame() {
-		
+
 		StageCanvas stageCanvas = new StageCanvas();
-		stageCanvas.start();
-		
-		this.setSize(500,500);
+
+		this.setSize(canvasWidth, canvasHeight);
 		this.setVisible(true);
-			
+
 		this.add(stageCanvas);
+
 	}
 }
