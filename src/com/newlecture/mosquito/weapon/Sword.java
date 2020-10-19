5c5a9a238f77cc788be0eaa1b48be212d5f2248f
hi <hi@DESKTOP-6RL4QDK>
2020-10-19 17:21:33 +0900

    [신중언] 무기(부채, 검, 창, 갓)클래스 기본구현

---
+++ src/com/newlecture/mosquito/weapon/Sword.java
@@ -0,0 +1,50 @@
+package com.newlecture.mosquito.weapon;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.newlecture.mosquito.canvas.StageCanvas;
+
+public class Sword {
+	private String type;
+	private int damage;
+	private double prob;
+	private Image img;
+	private int isClicked=0;
+	private double range = 1;
+	
+	private int w = 72;
+	private int h = 52;
+	private int x=0;
+	private int y=0;
+	
+	
+	public Sword() {
+		try {
+			img = ImageIO.read(new File(""));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		damage = 50;
+		type = "Sword";
+		prob = 1;
+	}
+	
+	public void paint(Graphics g) {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void cursor() {//마우스 위치에 따라 이동 이동
+	
+	}
+}
