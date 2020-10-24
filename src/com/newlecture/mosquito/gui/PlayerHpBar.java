06971f40ae0dfccffb61c603476a23874e44391c
junjun-creator <kimbj910210@naver.com>
2020-10-24 11:06:30 +0900

    [김병준] 플레이어 Hp 바 구현 완료

--- src/com/newlecture/mosquito/gui/PlayerHpBar.java
+++ src/com/newlecture/mosquito/gui/PlayerHpBar.java
@@ -13,7 +13,16 @@ public class PlayerHpBar {
 	public void paint(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.drawRect(800, 100, 300, 50);
-		g.setColor(Color.RED);
-		g.fillRect(800, 100, hp*3, 50);
+		
+		if(hp>=30)
+			g.setColor(Color.BLUE);
+		else
+			g.setColor(Color.RED);
+		
+		g.fillRect(800, 100, hp*3+1, 50+1);
+		
+		g.setColor(Color.BLACK);
+		g.drawString("HP : "+ hp + "%", 935, 130);
+		//g.drawString
 	}
 }
