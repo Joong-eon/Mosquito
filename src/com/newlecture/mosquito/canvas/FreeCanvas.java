d4e8a368fb7898908e6fa09b8927ccb146bb2c43
hi <hi@DESKTOP-6RL4QDK>
2020-10-26 14:44:51 +0900

    [신중언] 프리캔버스, 서비스

---
+++ src/com/newlecture/mosquito/canvas/FreeCanvas.java
@@ -0,0 +1,7 @@
+package com.newlecture.mosquito.canvas;
+
+import java.awt.Canvas;
+
+public class FreeCanvas extends Canvas{
+
+}
---
+++ src/com/newlecture/mosquito/service/FreeService.java
@@ -0,0 +1,5 @@
+package com.newlecture.mosquito.service;
+
+public class FreeService {
+
+}
